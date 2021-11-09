877a818eb42570f664cd5916e91da55f9f0dea50
DESKTOP-0PML5RT\hidak <hidakaryo115274@gmail.com>
2021-11-09 17:29:42 +0900

    Playerの動き

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody m_rigidbody;
+    [SerializeField] float m_movingSpeed = 5f;
+    [SerializeField] float m_isGroundedLength = 1f;
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        m_rigidbody.velocity = new Vector3(v * m_movingSpeed, m_rigidbody.velocity.y, h * m_movingSpeed);
+    }
+
+    bool IsGround()
+    {
+        Vector3 start = this.transform.position;
+        Vector3 end = start + Vector3.down * m_isGroundedLength;
+        Debug.DrawLine(start, end);
+        bool isGrounded = Physics.Linecast(start, end);
+        return isGrounded;
     }
 }
