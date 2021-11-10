0b43fa14c4bba8b8d9c33d29c255b1336c593a5d
DESKTOP-0PML5RT\hidak <hidakaryo115274@gmail.com>
2021-11-10 11:36:13 +0900

    Jumpの実装

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -7,9 +7,12 @@ public class Player : MonoBehaviour
     Rigidbody m_rigidbody;
     [SerializeField] float m_movingSpeed = 5f;
     [SerializeField] float m_isGroundedLength = 1f;
+    [SerializeField] float m_jumpPower = 5f;
+    Animator m_animator;
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,6 +22,12 @@ public class Player : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
 
         m_rigidbody.velocity = new Vector3(v * m_movingSpeed, m_rigidbody.velocity.y, h * m_movingSpeed);
+
+        if (Input.GetButtonDown("Jump") && IsGround())
+        {
+            m_rigidbody.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
+            m_animator.SetTrigger("Jump");
+        }
     }
 
     bool IsGround()
